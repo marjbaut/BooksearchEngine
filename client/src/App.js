539c0955f6407077d6e5890fa620bb78c23ec167
Marjorie <marjoriebaut.w@gmail.com>
2023-06-12 20:49:10 -0400

    fetch was called

--- client/src/App.js
+++ client/src/App.js
@@ -4,30 +4,44 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import LoginForm from './components/LoginForm';
 import Navbar from './components/Navbar';
 
-const client = new ApolloClient({
-    uri: '/graphql',
-    cache: new InMemoryCache(),
-});
+// const client = new ApolloClient({
+//     uri: '/graphql',
+//     cache: new InMemoryCache(),
+// });
 
+const SearchGoogleBooks = () => {
+const[searchGoogleBooks,setSearchGoogleBooks] = React.useState([])
+React.useEffect(()=> {
+    fetch('https://www.googleapis.com/books/v1/volumes?q=${query}')
+    .then(response=> response.json())
+    .then(searchGoogleBooks=> setSearchGoogleBooks(searchGoogleBooks))
+    .catch(err=>console.log(err))
+},[])
+    return (
+        <div>
+ 
+        </div>
+    )
+}
 function App() {
     return (
-        <ApolloProvider client={client}>
-            <div>hello</div>
-            
-        </ApolloProvider>
+        <ApolloProvider client={SearchGoogleBooks}>
+      <Router>
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<Navbar />}
+            />
+            <Route 
+              path="/"
+              element={<LoginForm />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
     );
 }
 
 export default App;
-
-// {/* //     <Router>
-//         //         <div className="flex-column justify-center align-center min-100-vh bg-primary">
-//         //             <Routes>
-//         //                 <Route 
-//         //                     path="/"
-//         //                     element=" "
-//         //                 />
-//         //             </Routes>
-//         //         </div>
-//         //     </Router> */}
-        
